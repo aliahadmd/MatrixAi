ca0dba081b9815636fe9d507299ad935b6ab032d
Ali <aliahadmd1@gmail.com>
2023-12-27 15:40:07 +0600

    bug fix: express  async handler install

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -1,70 +1,61 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-
+const asyncHandler = require("express-async-handler");
 
 //-------------Registration---------------
-const register=async (req,res, next)=>{
-   try {
-    const {username, email, password} = req.body;
+const register = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
 
-    //make sure user are providing data: validation
-    if (!username ||!email ||!password) {
-        return res.status(400);
-        throw new Error("Please provide all the required data");
-    }
+  //make sure user are providing data: validation
+  if (!username || !email || !password) {
+    return res.status(400);
+    throw new Error("Please provide all the required data");
+  }
 
-    //check if user already exists
-    const userExists = await User.findOne({ email });
-    if (userExists) {
-        res.status(400);
-        throw new Error("User already exists");
-    }
+  //check if user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
 
-    // Hash the password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+  // Hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-    //create the user
-    const newUser = new User({
-        username,
-        password:hashedPassword,
-        email,
-    })
+  //create the user
+  const newUser = new User({
+    username,
+    password: hashedPassword,
+    email,
+  });
 
-    // add the date the trial will end
-    newUser.trialExpires = new Date(
-      new Date().getTime() + newUser.trialPeriod * 24 * 60 * 60 * 1000
-    );
+  // add the date the trial will end
+  newUser.trialExpires = new Date(
+    new Date().getTime() + newUser.trialPeriod * 24 * 60 * 60 * 1000
+  );
 
-        // save the user
-        await newUser.save();
-     res.json({
-       status: true,
-       message: "Registration was Successfull",
-       user:{
-        username,
-        email,
-       }
-     });
-   } catch (error) {
-    next (error);
-   }
-};
+  // save the user
+  await newUser.save();
+  res.json({
+    status: true,
+    message: "Registration was Successfull",
+    user: {
+      username,
+      email,
+    },
+  });
+});
 
 //-------------Login---------------
 
-
 //-------------Logout---------------
-
 
 //-------------Profile---------------
 
-
 //-------------Check User Auth Status---------------
 
-
-
 // export
-module.exports={
-    register,
+module.exports = {
+  register,
 };
